afba5124c7c71ff63ec568707f1bd88be50c3ee5
adhithia-irvan <adhithia.rachmawan@agriaku.com>
2025-05-24 16:58:09 +0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM golang:latest AS builder
+WORKDIR /app
+COPY . .
+RUN go build -o myapp
+
+FROM alpine:latest
+WORKDIR /app
+COPY --from=builder /app/myapp .
+EXPOSE 8080
+ENTRYPOINT ["./myapp"]
+
+
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "success",
+			"version": "v3",
+			"code":    http.StatusOK,
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
